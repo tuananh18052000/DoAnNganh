57aac3650d161c0afaa99d49d9a16f6aea601574
Tuan Anh <bigskyanh123@gmail.com>
2021-08-08 19:20:08 +0700

    update JS

--- js/main.js
+++ js/main.js
@@ -25,3 +25,16 @@ var swiper = new Swiper(".banner-slider", {
     },
     loop:true,
   });
+
+  /* Image Comparison Slider*/
+  const slider = document.querySelector('.slider input');
+  const img = document.querySelector('.images .img-2');
+  const dragLine = document.querySelector('.slider .drag-line');
+
+  slider.oninput = () =>{
+    let sliderVal = slider.value;
+    dragLine.style.left = sliderVal + '%';
+    img.style.width = sliderVal + '%';
+  }
+  
+  
